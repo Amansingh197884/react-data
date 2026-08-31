5601fc74aeb13d1b8c31aaeb6db06a83432e93bb
Aman Singh <ta5638576@gmail.com>
2026-08-31 08:01:55 +0530

    commit

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import { BrowserRouter } from 'react-router-dom';
-import { PopupProvider } from './components/PopupContext.jsx'; // ✅ Correct Path
+import { PopupProvider } from './components/PopupContext.jsx';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
